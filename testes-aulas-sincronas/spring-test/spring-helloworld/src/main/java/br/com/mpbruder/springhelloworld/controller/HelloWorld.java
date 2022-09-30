b70a2706c3a87f23c85fd7586c29a3cb1ffc8178
matheusbruder <matheus.bruder@mercadolivre.com>
2022-09-30 12:18:52 -0300

    feat: hello world com spring boot

---
+++ testes-aulas-sincronas/spring-test/spring-helloworld/src/main/java/br/com/mpbruder/springhelloworld/controller/HelloWorld.java
@@ -0,0 +1,23 @@
+package br.com.mpbruder.springhelloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorld {
+
+    @GetMapping
+    public String getHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/nome/{nome}")
+    public String getHello(@PathVariable String nome) {
+        return "Hello, " + nome + "!";
+    }
+
+
+}
---
+++ testes-aulas-sincronas/spring-test/spring-helloworld/src/test/java/br/com/mpbruder/springhelloworld/SpringHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mpbruder.springhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
